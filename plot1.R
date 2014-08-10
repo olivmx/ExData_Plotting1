3b7c3579e64881f47740dbd0f57d91d6c5006bd8
unknown <olivmx@gmail.com>
2014-08-10 18:31:32 -0500

    project files

---
+++ plot1.R
@@ -0,0 +1,33 @@
+#plot 1 code
+
+
+1. READ THE CODE AND STRIP ALL LINES NOT USED
+
+setwd('C:/TestR/ExploratoryCoursera/Project1')
+
+#1.1 read file into dataframe named original and make sure it has data with the head command
+original <- data.frame(read.table('household_power_consumption.txt', header=TRUE, sep=";", dec=".", na.strings="?"))
+original$Date <- as.Date(original$Date , format="%d/%m/%Y")
+head (original$Date)
+
+#1.2 subset original dataframe into workingSet with only selected dates range and make 
+# sure it has the correct dates.
+workingSet<- subset(original, 
+                    original$Date >= as.Date('2007-02-01') & 
+                    original$Date <= as.Date('2007-02-02'))
+dim(workingSet)
+#1.3 remove original dataframe to free memory
+rm(original)
+
+#openfile
+png (filename="plot1.png", width=480, height=480)
+
+#create plot and format plot
+
+hist(workingSet$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+
+#close file
+dev.off()
+
+
+
---
+++ plot4.R
@@ -0,0 +1,47 @@
+#plot 1 code
+
+
+1. READ THE CODE AND STRIP ALL LINES NOT USED
+
+setwd('C:/TestR/ExploratoryCoursera/Project1')
+
+#1.1 read file into dataframe named original and make sure it has data with the head command
+original <- data.frame(read.table('household_power_consumption.txt', header=TRUE, sep=";", dec=".", na.strings="?"))
+original$Date <- as.Date(original$Date , format="%d/%m/%Y")
+head (original$Date)
+
+#1.2 subset original dataframe into workingSet with only selected dates range and make 
+# sure it has the correct dates.
+workingSet<- subset(original, 
+                    original$Date >= as.Date('2007-02-01') & 
+                         original$Date <= as.Date('2007-02-02'))
+dim(workingSet)
+#1.3 remove original dataframe to free memory
+rm(original)
+
+
+# 2. adjust data so time is ordered
+workingSet$Time[1]
+strptime(workingSet$Time[1], format="%H:%M:%S")
+dayHrMinNumeric<-
+     (as.numeric(substr(workingSet$Date,start=9, stop=10))-1)*24+
+     as.numeric(substr(workingSet$Time, start=1, stop=2))+as.numeric(substr(workingSet$Time, start=4, stop=5))/60
+#3. construct plot
+
+#openfile
+png (filename="plot1.png", width=480, height=480)
+
+op <- par(mfrow = c(2, 2), # 2 x 2 pictures on one plot
+          pty = "s")       # square plotting region,
+# independent of device size
+plot()
+lines(dayHrMinNumeric, workingSet$Sub_metering_1, type="l", col="black")
+lines(dayHrMinNumeric, workingSet$Sub_metering_2, type="l", col="red")
+lines(dayHrMinNumeric, workingSet$Sub_metering_3, type="l", col="blue")
+
+#4. format plot
+
+
+
+#close file
+dev.off()
